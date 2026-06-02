5b4266e4882ea0bfa099962151ffd6e355db9180
(Lê Vũ Hùng) <levuhung678@gmail.com>
2026-06-02 23:45:45 +0700

    tạo cấu trúc dự án cho CA server

---
+++ src/main/java/com/ca/ca_server/CaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ca.ca_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaServerApplication.class, args);
+	}
+
+}
